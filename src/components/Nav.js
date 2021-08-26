cec1b9e040c1c348abb2f74a070aaa04d1e857b3
george-shammar <george.gbenle@outlook.com>
2021-08-26 10:00:52 +0000

    Implement nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,1 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+    return (
+        <div>
+          <Link to="/" className="links">Home </Link>
+        </div>
+    );
+  }
+  
+  export default Nav;
