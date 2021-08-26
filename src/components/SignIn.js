02480ce07845e8f67eb66b10086e75553ea77a11
george-shammar <george.gbenle@outlook.com>
2021-08-26 09:27:46 +0000

    Style SignIn component

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -5,7 +5,11 @@ const SignIn = () => {
     return (
             <div className="wrapper fadeInDown">
                 <div className="formContent">
-                <p><span className="material-icons logo">maps_ugc</span>health <span className="p-subtitle">Track</span></p>
+                <div className="health">
+                    <span className="material-icons logo">maps_ugc</span>
+                    <p>Health<span className="p-subtitle">Track</span></p>
+                </div>
+                <h3 className="sign-in">Sign In</h3>
                 <div className="form">
                     <input type="text" className="fadeIn second" name="login" placeholder="username"></input>
                     <button type="button" className="fadeIn fourth">Log In</button>
