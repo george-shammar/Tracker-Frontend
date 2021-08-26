748b234083fc0f35af3aebc6158c059bd4a35964
george-shammar <george.gbenle@outlook.com>
2021-08-26 08:18:18 +0000

    Style SignIn component

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -3,23 +3,16 @@ import '../stylesheets/SignIn.css';
 
 const SignIn = () => {
     return (
-        
             <div className="wrapper fadeInDown">
-                <div id="formContent">
-                <h2 className="active"> Sign In </h2>
-                <h2 class="inactive underlineHover">Sign Up </h2>
-                <form>
-                    <input type="text" id="login" class="fadeIn second" name="login" placeholder="login"></input>
-                    <input type="text" id="password" class="fadeIn third" name="login" placeholder="password"></input>
-                    <input type="submit" class="fadeIn fourth" value="Log In"></input>
+                <div className="formContent">
+                <p><span className="material-icons logo">maps_ugc</span>health <span className="p-subtitle">Track</span></p>
+                <form className="form">
+                    <input type="text" id="login" className="fadeIn second" name="login" placeholder="username"></input>
+                    <input type="submit" className="fadeIn fourth" value="Log In"></input>
                 </form>
                 </div>
-                <div id="formFooter">
-                    <a class="underlineHover" href="#">Forgot Password?</a>
-                </div>
             </div>
-        
-    )
+    ) 
 }
 
 export default SignIn;
