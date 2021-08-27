0e3a4e7cfa0ca726c85f0d5ba60c5ea5d5139ef2
george-shammar <george.gbenle@outlook.com>
2021-08-27 09:00:08 +0000

    Style cursor

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,7 +11,7 @@ const Nav = () => {
             <p>Health<span className="p-subtitle">Track</span></p>
           </div>
           </Link>
-          <Link to="/" className="log-out"><i class="far fa-user"></i></Link>
+          <Link to="/" className="log-out"><i className="far fa-user"></i></Link>
         </div>
     );
   }
