b2a3fff1f2b7ec801a081b683bc32ec12b99e8ce
george-shammar <george.gbenle@outlook.com>
2021-08-31 19:44:40 +0000

    Style measurement component

--- src/components/Measurement.js
+++ src/components/Measurement.js
@@ -10,10 +10,6 @@ const Measurement = () => {
             <p>Tracker</p>
             <div className="wrapper fadeInDown">
                 <div className="formContent">
-                <div className="health">
-                    <span className="material-icons logo">maps_ugc</span>
-                    <p>Health<span className="p-subtitle">Track</span></p>
-                </div>
                 <div className="form">
                     <input type="text" className="fadeIn second" name="login" placeholder="Blood Pressure"></input>
                     <input type="text" className="fadeIn second" name="login" placeholder="Blood Sugar"></input>
