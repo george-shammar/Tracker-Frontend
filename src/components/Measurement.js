9e7327a443f115f76593abee1e301c7edac0f908
george-shammar <george.gbenle@outlook.com>
2021-08-31 19:42:34 +0000

    Implement routes

--- src/components/Measurement.js
+++ src/components/Measurement.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Header from './Header';
 import Nav from './Nav';
+import '../stylesheets/measurement.css';
 
 const Measurement = () => {
     return (
         <div>
             <Header />
-            <p>Measurement</p>
+            <p>Tracker</p>
+            <div className="wrapper fadeInDown">
+                <div className="formContent">
+                <div className="health">
+                    <span className="material-icons logo">maps_ugc</span>
+                    <p>Health<span className="p-subtitle">Track</span></p>
+                </div>
+                <div className="form">
+                    <input type="text" className="fadeIn second" name="login" placeholder="Blood Pressure"></input>
+                    <input type="text" className="fadeIn second" name="login" placeholder="Blood Sugar"></input>
+                    <button type="button" className="fadeIn fourth">Submit</button>
+                </div>
+                </div>
+            </div>
             <Nav />
         </div>
     )
