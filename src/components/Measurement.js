4f3187739ab80e1e497b1c7780ab01dbc8dbec07
george-shammar <george.gbenle@outlook.com>
2021-08-31 19:14:34 +0000

    Implement measurement.js

---
+++ src/components/Measurement.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Measurement = () => {
+    return (
+        <div>
+            <p>Measurement</p>
+        </div>
+    )
+    
+}
+
+export default Measurement;
