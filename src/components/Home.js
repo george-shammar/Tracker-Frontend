4c98dacdca02a8b91e1d4572bc8a4f0f0926c556
george-shammar <george.gbenle@outlook.com>
2021-08-31 12:28:56 +0000

    Implement Blood Pressure

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ const Home = () => {
     return (
         <div>
             <p>Date</p>
-            <img className="heart-img" src='https://media.istockphoto.com/vectors/anatomical-heart-isolated-heart-diagnostic-center-sign-human-heart-vector-id1177145926?k=20&m=1177145926&s=612x612&w=0&h=30PszSkc9jmobfif-KVcCaauGUDAWlGspthpmtMsI54=' />
-            <p>Blood Pressure</p>
+            
+            <p><img className="heart-img" src='https://media.istockphoto.com/vectors/anatomical-heart-isolated-heart-diagnostic-center-sign-human-heart-vector-id1177145926?k=20&m=1177145926&s=612x612&w=0&h=30PszSkc9jmobfif-KVcCaauGUDAWlGspthpmtMsI54=' />Blood Pressure</p>
             <p>Blood Glucose</p>
         </div>
     )
