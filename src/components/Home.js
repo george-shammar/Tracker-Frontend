7958646ae37f3e76712ceeeec2df0e46cb098be9
george-shammar <george.gbenle@outlook.com>
2021-08-31 19:05:56 +0000

    Add blood sugar image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../stylesheets/home.css';
-const bloodSugar = ''
+const bloodSugar = 'https://st.depositphotos.com/1008836/1993/i/950/depositphotos_19931429-stock-photo-glass-blood-drop-isolated.jpg'
 const bloodPressure = 'https://media.istockphoto.com/vectors/anatomical-heart-isolated-heart-diagnostic-center-sign-human-heart-vector-id1177145926?k=20&m=1177145926&s=612x612&w=0&h=30PszSkc9jmobfif-KVcCaauGUDAWlGspthpmtMsI54=';
 
 const Home = () => {
@@ -9,10 +9,20 @@ const Home = () => {
             <p>Add Metrics</p>
             <div className="add-metrics">
                 <div className="heart">
-                    <p><img className="heart-img" src={bloodPressure} />Blood Pressure</p>
+                    <p><img className="heart-img" src={bloodPressure} alt=''/>Blood Pressure</p>
                 </div>
                 <div  className="heart">
-                    <p><img className="heart-img" src={bloodSugar} />Blood Glucose</p>
+                    <p><img className="heart-img" src={bloodSugar} alt=''/>Blood Glucose</p>
+                </div>
+            </div>
+
+            <hr />
+            <div>
+                <h5>Your tracked health metrics</h5>
+                <div className="metrics">
+                    <p>Date</p>
+                    <p>Blood Pressure</p>
+                    <p>Blood Glucose</p>
                 </div>
             </div>
         </div>
