7f2ec76be034ee62e26b06aef4c8c4d31905a511
george-shammar <george.gbenle@outlook.com>
2021-09-04 22:36:04 +0000

    Implement newTracker action

--- src/action/index.js
+++ src/action/index.js
@@ -1,8 +1,12 @@
-import { createUser } from '../api';
+import { createTracker, createUser } from '../api';
 
 const newUser = (username) => async (dispatch) => {
     const payload = await createUser(username);
     dispatch({ type: 'signIn/newUser', payload });
-  };
+};
 
-export { newUser };
+const newTracker = (blood_pressure, blood_glucose, id) => async (dispatch) => {
+  const payload = await createTracker(blood_pressure, blood_glucose, id);
+}
+
+export { newUser, newTracker };
