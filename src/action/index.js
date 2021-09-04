77bbdf54d5b3d2fea2c80de2386d8432855246b4
george-shammar <george.gbenle@outlook.com>
2021-09-04 16:08:02 +0000

    Create sign in reducer

--- src/action/index.js
+++ src/action/index.js
@@ -1,1 +1,8 @@
 import { createUser } from '../api';
+
+const newUser = () => async (dispatch) => {
+    const payload = await createUser(username);
+    dispatch({ type: 'signIn/newUser', payload });
+  };
+
+export { newUser };
