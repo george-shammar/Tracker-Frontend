f73e3a84898773a8f4ce36bb8c708d68d4ccfdc5
george-shammar <george.gbenle@outlook.com>
2021-09-04 22:33:20 +0000

    Implement Tracker API call

--- src/api.js
+++ src/api.js
@@ -2,7 +2,6 @@ import 'regenerator-runtime/runtime';
 
 const createUser = async (username) => {
     try {
-      console.log("it works");
       const response = await fetch(`https://fierce-garden-46227.herokuapp.com/api/users/`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json',
@@ -17,4 +16,22 @@ const createUser = async (username) => {
     }
 };
 
-export { createUser };
+const createTracker = async (blood_pressure, blood_glucose, id) => {
+  try {
+    const response = await fetch(`https://fierce-garden-46227.herokuapp.com/api/trackers/`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json',
+                Accept: 'application/json' },
+      body: JSON.stringify(blood_pressure, blood_glucose, id),
+    });
+    
+    return response.json();
+    
+  } catch (error) {
+    return error.message;
+  }
+};
+
+
+
+export { createUser, createTracker };
