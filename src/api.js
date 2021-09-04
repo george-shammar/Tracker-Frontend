2d56c50889a9a1ff51476c9dc67ea7fc582cec28
george-shammar <george.gbenle@outlook.com>
2021-09-04 15:24:37 +0000

    Implement createUser API call

--- src/api.js
+++ src/api.js
@@ -1,1 +1,17 @@
 import 'regenerator-runtime/runtime';
+
+const createUser = async (username) => {
+    try {
+      const response = await fetch(`https://fierce-garden-46227.herokuapp.com/api/users/`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json',
+                  Accept: 'application/json' },
+        body: JSON.stringify(username),
+      });
+      return response.json();
+    } catch (error) {
+      return error.message;
+    }
+};
+
+export { createUser };
