8134736ef51298af9f1987c9ccef2559045db575
george-shammar <george.gbenle@outlook.com>
2021-09-04 16:13:19 +0000

    Implement sign in reducer

--- src/reducers/SignInReducer.js
+++ src/reducers/SignInReducer.js
@@ -1,1 +1,12 @@
 const initialState = "";
+
+const signInReducer = (username = initialState, action) => {
+    switch (action.type) {
+      case 'signIn/newUser':
+        return action.payload;
+      default:
+        return username;
+    }
+};
+
+export default signInReducer;
