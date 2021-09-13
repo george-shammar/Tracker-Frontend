03943bc802afe314b0b2cf51cb793780981abf82
george-shammar <george.gbenle@outlook.com>
2021-09-13 10:41:55 +0000

    Create Style.css

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SearchTerm from '../containers/Search';
 import { useDispatch } from 'react-redux';
+import { setSearchTerm, clearSearchTerm } from '../action/index';
 import '../stylesheets/home.css';
 const bloodSugar = 'https://st.depositphotos.com/1008836/1993/i/950/depositphotos_19931429-stock-photo-glass-blood-drop-isolated.jpg'
 const bloodPressure = 'https://media.istockphoto.com/vectors/anatomical-heart-isolated-heart-diagnostic-center-sign-human-heart-vector-id1177145926?k=20&m=1177145926&s=612x612&w=0&h=30PszSkc9jmobfif-KVcCaauGUDAWlGspthpmtMsI54=';
