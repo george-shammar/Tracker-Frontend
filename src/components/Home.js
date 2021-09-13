7499032ae16f2434f67c13f0ada6e4b95cb26446
george-shammar <george.gbenle@outlook.com>
2021-09-13 09:32:23 +0000

    Refactor Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,18 +6,7 @@ import '../stylesheets/home.css';
 const bloodSugar = 'https://st.depositphotos.com/1008836/1993/i/950/depositphotos_19931429-stock-photo-glass-blood-drop-isolated.jpg'
 const bloodPressure = 'https://media.istockphoto.com/vectors/anatomical-heart-isolated-heart-diagnostic-center-sign-human-heart-vector-id1177145926?k=20&m=1177145926&s=612x612&w=0&h=30PszSkc9jmobfif-KVcCaauGUDAWlGspthpmtMsI54=';
 
-const Home = (props) => {
-    const userId = props.match.params.id;
-    const dispatch = useDispatch();
-    
-    const getTracker = (userId) => {
-        dispatch(getUserTrackers(userId));
-    };
-
-    React.useEffect(() => {
-        getTracker(userId);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+const Home = () => {
 
     return (
         <div className="home">
