6b50a958382c632a47735613a17f1d464a0745a0
george-shammar <george.gbenle@outlook.com>
2021-09-15 10:08:09 +0000

    Create

--- src/action/index.js
+++ src/action/index.js
@@ -1,9 +1,13 @@
-import { createUser, fetchTrackers } from '../api';
+import { createUser, fetchTrackers, createTracker } from '../api';
 
 const newUser = (username) => async (dispatch) => {
     const payload = await createUser(username);
     dispatch({ type: 'signIn/newUser', payload });
 };
+
+const newTracker = (blood_pressure, blood_glucose, user_id) => async (dispatch) => {
+    
+}
 
 const loadData = () => async (dispatch) => {
     const payload = await fetchTrackers();
