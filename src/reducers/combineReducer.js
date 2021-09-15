e6753b2137b281a3f20ed7ac843fcf3905303ebd
george-shammar <george.gbenle@outlook.com>
2021-09-15 10:01:11 +0000

    Implement createTracker in combine Reducer

--- src/reducers/combineReducer.js
+++ src/reducers/combineReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import allTrackersReducer from './getTrackerReducer';
 import signInReducer from './SignInReducer';
+import createTrackerReducer from './createTrackerReducer';
 
 const reducers = {
   username: signInReducer,
   allTrackers: allTrackersReducer,
+  tracker: createTrackerReducer,
 };
 
 const rootReducer = combineReducers(reducers);
