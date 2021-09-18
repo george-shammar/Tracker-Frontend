165b3030967925cbc9169869c612ca4ccec2ed61
george-shammar <george.gbenle@outlook.com>
2021-09-18 20:06:49 +0000

    Refactor Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,61 +1,73 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
 import Tracker from './Tracker';
-import { loadData, selectTrackers } from '../action/index';
 import '../stylesheets/home.css';
 
 const bloodSugar = 'https://st.depositphotos.com/1008836/1993/i/950/depositphotos_19931429-stock-photo-glass-blood-drop-isolated.jpg';
 const bloodPressure = 'https://media.istockphoto.com/vectors/anatomical-heart-isolated-heart-diagnostic-center-sign-human-heart-vector-id1177145926?k=20&m=1177145926&s=612x612&w=0&h=30PszSkc9jmobfif-KVcCaauGUDAWlGspthpmtMsI54=';
 
-const Home = () => {
-  const allTrackers = useSelector(selectTrackers);
-  const dispatch = useDispatch();
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tracker: null,
+    };
+  }
 
-  const onFirstRender = () => {
-    dispatch(loadData());
-  };
+  componentDidMount() {
+    fetch('https://fierce-garden-46227.herokuapp.com/api/trackers/')
+      .then((resp) => resp.json())
+      .then((json) => this.setState({ tracker: json }));
+  }
 
-  useEffect(onFirstRender, []);
-
-  return (
-    <div className="home">
-      <p>Add Metrics</p>
-      <div className="add-metrics">
-        <div className="heart">
-          <p>
-            <img className="heart-img" src={bloodPressure} alt="" />
-            <Link to="/Measurement">Blood Pressure</Link>
-          </p>
+  render() {
+    const { tracker } = this.state;
+    return (
+      <div className="home">
+        <p>Add Metrics</p>
+        <div className="add-metrics">
+          <div className="heart">
+            <p>
+              <img className="heart-img" src={bloodPressure} alt="" />
+              <Link to="/Measurement">Blood Pressure</Link>
+            </p>
+          </div>
+          <div className="heart">
+            <p>
+              <img className="heart-img" src={bloodSugar} alt="" />
+              <Link to="/Measurement">Blood Glucose</Link>
+            </p>
+          </div>
         </div>
-        <div className="heart">
-          <p>
-            <img className="heart-img" src={bloodSugar} alt="" />
-            <Link to="/Measurement">Blood Glucose</Link>
-          </p>
-        </div>
-      </div>
 
-      <hr />
-      <div>
-        <h5>Your health track</h5>
+        <hr />
+        <div>
+          <h5>Your health track</h5>
+          {tracker !== null && tracker.data.length > 0
+          && (
+            <div>
+                {tracker.data.map((tracker) => (
+                  <Tracker tracker={tracker} key={tracker.id} />
+                ))}
+            </div>
+          )}
+          {/* {allTrackers.data.length > 0 ? ( */}
 
-        {allTrackers.data.length > 0 ? (
-
-          <div>
+          {/* <div>
             {allTrackers.data.map((tracker) => (
               <Tracker tracker={tracker} key={tracker.id} />
             ))}
 
-          </div>
-
+          </div> */}
+          {/*
         ) : (
           <p> Loading data...</p>
-        )}
-      </div>
+        )} */}
+        </div>
 
-    </div>
-  );
-};
+      </div>
+    );
+  }
+}
 
 export default Home;
