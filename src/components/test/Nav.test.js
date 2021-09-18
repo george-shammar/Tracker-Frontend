b7f1ca947ea6867c17153cc8b30c96ea794ff1b3
george-shammar <george.gbenle@outlook.com>
2021-09-18 12:30:02 +0000

    Write test for nav component

---
+++ src/components/test/Nav.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import Nav from '../Nav';
+
+it('renders Nav without crashing', () => {
+    const div = document.createElement('div');
+  
+    ReactDom.render(
+      <BrowserRouter>
+        <Nav />
+      </BrowserRouter>,
+      div,
+    );
+  
+    ReactDom.unmountComponentAtNode(div);
+  });
