fa2b47bd13942e0afe2a5449127680f06a06f7c1
george-shammar <george.gbenle@outlook.com>
2021-09-18 12:47:24 +0000

    Create test for sign in reducers

---
+++ src/components/test/signInReducer.test.js
@@ -0,0 +1,13 @@
+import signInReducer from '../../reducers/SignInReducer';
+
+test('should return the initial state', () => {
+    expect(signInReducer(undefined, {})).toEqual("");
+});
+
+test('should return the initial state with empty string', () => {
+    expect(signInReducer(undefined, {})).not.toEqual("username");
+});
+
+test('should return a defined initial state', () => {
+    expect(signInReducer(undefined, {})).toBeDefined();
+});
