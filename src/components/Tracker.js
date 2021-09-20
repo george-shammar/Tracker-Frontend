420c98cd6a4f1fdebec15fb308fc813a5042a945
george-shammar <george.gbenle@outlook.com>
2021-09-20 13:30:31 +0000

    Style measurement images

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -5,17 +5,23 @@ import '../stylesheets/tracker.css';
 const Tracker = ({ tracker }) => (
   <div className="tracker">
     <div className="card">
-      <p className="text-unit">Blood Pressure:</p>
-      <div className="display">
-        <p className="figure">{tracker.blood_pressure}</p>
-        <p className="unit-two">mmHg</p>
+      <i className="fas heart fa-heartbeat" />
+      <div>
+        <p className="text-unit">Blood Pressure:</p>
+        <div className="display">
+          <p className="figure">{tracker.blood_pressure}</p>
+          <p className="unit-two">mmHg</p>
+        </div>
       </div>
     </div>
     <div className="card">
-      <p className="text-unit">Blood Glucose:</p>
-      <div className="display">
-        <p className="figure">{tracker.blood_glucose}</p>
-        <p className="unit-two">mmol/L</p>
+      <i className="fas heart fa-signal" />
+      <div>
+        <p className="text-unit">Blood Glucose:</p>
+        <div className="display">
+          <p className="figure">{tracker.blood_glucose}</p>
+          <p className="unit-two">mmol/L</p>
+        </div>
       </div>
     </div>
   </div>
